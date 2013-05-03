55d2dd078b375f6bc51edb44c13cab5e9bd1465d
Herbert Gartner <herbert.gartner@silverserver.at>
2013-05-03 18:02:25 +0200

    Initial Commit

---
+++ MalVersuch/HGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HGAppDelegate.h
+//  MalVersuch
+//
+//  Created by Herbert Gartner on 03.05.13.
+//  Copyright (c) 2013 Herbert Gartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MalVersuch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MalVersuch
+//
+//  Created by Herbert Gartner on 03.05.13.
+//  Copyright (c) 2013 Herbert Gartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HGAppDelegate class]));
+    }
+}
